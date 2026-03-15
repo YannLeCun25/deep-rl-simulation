43070c1b37ca6173f8d108281c0c93f69c63d3df
Yann LeCun <kamuhina733@gmail.com>
2026-03-15 19:35:20 +0800

    Inject advanced World-Class AI algorithms and kernels

---
+++ src/buffers/replay_buffer.py
@@ -0,0 +1,13 @@
+import random
+from collections import deque
+
+class ReplayBuffer:
+    """Experience Replay Buffer for off-policy RL training."""
+    def __init__(self, capacity: int = 10000):
+        self.buffer = deque(maxlen=capacity)
+
+    def push(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size: int):
+        return random.sample(self.buffer, batch_size)
